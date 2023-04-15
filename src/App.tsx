fa6ed1f8a80984a022622782a74e8a594e2d477d
Santiago Benito <bmsanti@hotmail.com>
2023-04-15 23:07:54 +0200

    navbar_v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Main } from "./views/Main/Main"
+import "../src/styles/settings/globals.scss";
+
+import { Main } from "./views/Main/Main";
 
 const App: React.FC = () => {
-  return (
-    <Main />
-  )
-}
+  return <Main />;
+};
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,59 @@
-import './Navbar.scss'
+import { useState } from "react";
+import "./Navbar.scss";
+import logo from "../../assets/img/logo.png";
+import { IonIcon } from "@ionic/react";
+import { menu, close, search } from "ionicons/icons";
 
 export const Navbar = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [showSearchBox, setShowSearchBox] = useState<boolean>(false);
+
+  const handleMenu = () => {
+    setShowSearchBox(false);
+    setShowMenu((prev) => !prev);
+  };
+
+  const handleSearchBox = () => {
+    setShowMenu(false);
+    setShowSearchBox((prev) => !prev);
+  };
+
   return (
-    <nav>
-      <img src="../assets" alt="" />
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-      </ul>
+    <nav className="navbar">
+      <img src={logo} alt="movie app logo" className="logo" />
 
-  
+      {showSearchBox && (
+        <form>
+          <input
+            type="text"
+            placeholder="Buscar Película"
+            className="searchBoxInput"
+          />
+        </form>
+      )}
 
+      <div className="iconsWrapper">
+        <IonIcon
+          icon={search}
+          className="searchIcon"
+          onClick={handleSearchBox}
+        />
 
+        <IonIcon
+          icon={showMenu ? close : menu}
+          className="menuIcon"
+          onClick={handleMenu}
+        />
+      </div>
+
+      {showMenu && (
+        <ul className="menu">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      )}
     </nav>
-  )
-}
+  );
+};
