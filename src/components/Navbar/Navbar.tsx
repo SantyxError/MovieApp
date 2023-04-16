46b84f0a501a4774b351fa351f656f1cfa7d3010
Santiago Benito <bmsanti@hotmail.com>
2023-04-16 11:19:10 +0200

    navbar_v2.0

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,58 +2,28 @@ import { useState } from "react";
 import "./Navbar.scss";
 import logo from "../../assets/img/logo.png";
 import { IonIcon } from "@ionic/react";
-import { menu, close, search } from "ionicons/icons";
+import { search } from "ionicons/icons";
 
 export const Navbar = () => {
-  const [showMenu, setShowMenu] = useState<boolean>(false);
-  const [showSearchBox, setShowSearchBox] = useState<boolean>(false);
-
-  const handleMenu = () => {
-    setShowSearchBox(false);
-    setShowMenu((prev) => !prev);
-  };
-
-  const handleSearchBox = () => {
-    setShowMenu(false);
-    setShowSearchBox((prev) => !prev);
-  };
-
   return (
     <nav className="navbar">
       <img src={logo} alt="movie app logo" className="logo" />
 
-      {showSearchBox && (
-        <form>
-          <input
-            type="text"
-            placeholder="Buscar Película"
-            className="searchBoxInput"
-          />
-        </form>
-      )}
-
-      <div className="iconsWrapper">
-        <IonIcon
-          icon={search}
-          className="searchIcon"
-          onClick={handleSearchBox}
-        />
-
-        <IonIcon
-          icon={showMenu ? close : menu}
-          className="menuIcon"
-          onClick={handleMenu}
-        />
-      </div>
-
-      {showMenu && (
+      <div className="menuWrapper">
         <ul className="menu">
           <li>Item 1</li>
           <li>Item 2</li>
           <li>Item 3</li>
           <li>Item 4</li>
         </ul>
-      )}
+
+        <form className="searchWrapper">
+          <input placeholder="Buscar película" className="searchBox" />
+          <button type="submit" className="searchButton" aria-label="Búsqueda">
+            <IonIcon icon={search} />
+          </button>
+        </form>
+      </div>
     </nav>
   );
 };
