6fe793184331e7ce02b1a4cebd60116357fe5b2a
Santiago Benito <sanbemal@gmail.com>
2023-04-18 17:10:35 +0200

    Añadidas todas las listas del navBar

---
+++ src/views/PopularFilms/PopularFilms.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import styles from "./PopularFilms.module.scss";
+import { Films } from "../../models/Films.model";
+import { FilmCard } from "../../components/FilmCard/FilmCard";
+import { Spinner } from "../../components/Spinner/Spinner";
+
+
+
+
+export const PopularFilms = () => {
+  const [data, setData] = useState<Films | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const key = import.meta.env.VITE_API_KEY;
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const url = `${baseUrl}/movie/popular?api_key=${key}&language=es-ES`;
+
+  const imgUrl = import.meta.env.VITE_IMG_BASE;
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setData(data);
+    } catch (error) {
+      console.log("Error:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div className={styles.spinnerContainer}>
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (data === undefined) {
+    return (
+      <div className={styles.spinnerContainer}>
+        No se ha podido resolver la petición
+      </div>
+    );
+  }
+
+  return (
+    <section className={styles.container}>
+       <h2 className={styles.title}>Películas Populares</h2>
+       <div className={styles.filmContainer}>
+      {data.results.map((film) => {
+        return <FilmCard imgUrl={imgUrl} film={film} />;
+      })}
+       </div>
+    </section>
+  )
+}
+
+
