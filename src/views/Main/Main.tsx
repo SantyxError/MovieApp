10305b6966c6a2bd7d0a8862b01bb3bb9465a644
Santiago Benito <sanbemal@gmail.com>
2023-04-18 12:55:40 +0200

    Añadido listado "Peliculas Populares"

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -2,6 +2,7 @@ import { BottomNavbar } from "../../components/BottomNavbar/BottomNavbar";
 import { Navbar } from "../../components/Navbar/Navbar";
 import styles from "./Main.module.scss";
 import { DiscoverFilms } from "../DiscoverFilms/DiscoverFilms";
+import { TopFilms } from "../TopFilms/TopFilms";
 
 export const Main = () => {
   return (
@@ -9,7 +10,8 @@ export const Main = () => {
       <Navbar />
       <main className={styles.main}>
        
-        <DiscoverFilms />
+        {/* <DiscoverFilms /> */}
+        <TopFilms/>
       </main>
       <footer>footer</footer>
       <BottomNavbar />
