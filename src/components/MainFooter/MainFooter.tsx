90aad0c6c93777b193afb6850b8959720bf5353c
Santiago Benito <sanbemal@gmail.com>
2023-04-20 13:30:44 +0200

    Version desktop en desarrollo

--- src/components/MainFooter/MainFooter.tsx
+++ src/components/MainFooter/MainFooter.tsx
@@ -3,7 +3,7 @@ import styles from "./MainFooter.module.scss";
 export const MainFooter = () => {
   return (
     <div className={styles.container}>
-      <p className={styles.text}>Developed by Santiago Benito. 2023</p>
+      <p className={styles.text}>2023 Copyright © Santiago Benito. All rights reserved.</p>
       </div>
   );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,18 +2,40 @@ import styles from "./Navbar.module.scss";
 import logo from "../../assets/img/logo.png";
 import { IonIcon } from "@ionic/react";
 import { search } from "ionicons/icons";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+
+const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate("/")
+  }
+
+  const navigateToFilms = () => {
+    navigate("/peliculas")
+  }
+
+  const navigateToTV = () => {
+    navigate("/tv")
+  }
+
+  const navigateToTop = () => {
+    navigate("/top")
+  }
+
+
+
   return (
     <nav className={styles.navbar}>
       <img src={logo} alt="movie app logo" className={styles.logo} />
 
       <div className={styles.menuWrapper}>
         <ul className={styles.menu}>
-          <li className={styles.item}>Item 1</li>
-          <li className={styles.item}>Item 2</li>
-          <li className={styles.item}>Item 3</li>
-          <li className={styles.item}>Item 4</li>
+          <li className={styles.item} onClick={navigateToHome}>Últimas Películas</li>
+          <li className={styles.item}onClick={navigateToFilms}>Películas más Populares</li>
+          <li className={styles.item}onClick={navigateToTV}>Programas más Populares</li>
+          <li className={styles.item}onClick={navigateToTop}>Películas más Valoradas</li>
         </ul>
 
         <form className={styles.searchWrapper}>
